e6fd7d7f614a9910f6d0093021800528c8fe7011
Pete Hollobon <pete@hollobon.com>
2016-02-11 09:27:05 +0000

    Implement remaining instructions; add CGRAM example

--- 1602a_example.c
+++ 1602a_example.c
@@ -3,6 +3,17 @@
 
 #include "1602a_lcd.h"
 
+uint8_t smiley[] = {
+    0b01110,
+    0b10001,
+    0b11011,
+    0b10001,
+    0b10101,
+    0b10001,
+    0b01110,
+    0b00000,
+};
+
 int main(void)
 {
     // Set up outputs for shift register
@@ -10,24 +21,31 @@ int main(void)
     DDRE |= _BV(6);
 
     // Set up outputs for E / RS
-    DDRD |= _BV(0) | _BV(1); 
+    DDRD |= _BV(0) | _BV(1);
 
     _delay_ms(200);
-    write_1602a(false, 0b00111000); // function set - 8 bit, 2 lines, 5x11
+    function_set_1602a(true, true, false);
     clear_1602a();
     displayon_1602a(true, true);
-    write_1602a(false, 0b00000110); // entry mode set
+    entrymode_1602a(true, false);
+
+    set_cgaddress_1602a(0, 0);
+    for (int8_t i = 0; i < 8; i++)
+        putc_1602a(smiley[i]);
+
+    set_address_1602a(0, 0);
     puts_1602a("Test");
-    setaddress_1602a(0, 1);
-    puts_1602a("Line 2");
+    set_address_1602a(0, 1);
+    puts_1602a("Line 2 ");
+    putc_1602a(0);
 
     const char *spinner = "_oOo";
     const char *current = spinner;
     while (1) {
-        setaddress_1602a(10, 1);
+        set_address_1602a(10, 1);
         putc_1602a(*current++);
         if (!*current)
             current = spinner;
-        _delay_ms(100);
+        _delay_ms(200);
     }
 }
--- 1602a_lcd.h
+++ 1602a_lcd.h
@@ -3,15 +3,31 @@
 
 #include <avr/io.h>
 
+#define clear_1602a() do { write_1602a(false, 0x1); _delay_ms(2); } while (0)
+
+#define home_1602a() do { write_1602a(false, 0x2); _delay_ms(2); } while (0)
+
+#define entrymode_1602a(cursor_increment, shift_display) write_1602a(false, 0x4 | ((cursor_increment) ? 2 : 0) | ((shift_display) ? 1 : 0))
+
+#define displayon_1602a(cursor, cursor_blink) write_1602a(false, 0xc | ((cursor) ? 2 : 0) | ((cursor_blink) ? 1 : 0))
+
+#define displayoff_1602a() write_1602a(false, 0x8)
+
+#define cursor_left_1602a write_1602a(false, 0b00010000)
+
+#define cursor_right_1602a write_1602a(false, 0b00010100)
+
+#define display_left_1602a write_1602a(false, 0b00011000)
+
+#define display_right_1602a write_1602a(false, 0b00011100)
+
+#define function_set_1602a(data_8bit, lines_2, large_font) write_1602a(false, 0x20 | ((data_8bit) ? 0x10 : 0) | ((lines_2) ? 0x8 : 0) | ((large_font) ? 0x4 : 0))
+
+#define set_cgaddress_1602a(character, line) write_1602a(false, 0x40 | ((character) << 3) | ((line) & 0x7))
+
+#define set_address_1602a(x, y) write_1602a(false, 0x80 + ((y) * 0x40) + (x))
+
 #define putc_1602a(c) write_1602a(true, (c))
-
-#define clear_1602a() do { write_1602a(false, 0b00000001); _delay_ms(2); } while (0)
-
-#define setaddress_1602a(x, y) write_1602a(false, 0x80 + ((y) * 0x40) + (x))
-
-#define displayon_1602a(cursor, cursor_blink) write_1602a(false, 8 | 4 | ((cursor) ? 2 : 0) | ((cursor_blink) ? 1 : 0))
-
-#define displayoff_1602a() write_1602a(false, 8)
 
 void puts_1602a(const char *str);
 
